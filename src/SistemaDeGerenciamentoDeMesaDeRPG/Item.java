4b2d8023d5706edf21cc7d921068f3a7028c6ac3
Jaqueline Silva <js654367@gmail.com>
2026-09-09 11:33:15 -0300

    correção de métodos

--- src/SistemaDeGerenciamentoDeMesaDeRPG/Item.java
+++ src/SistemaDeGerenciamentoDeMesaDeRPG/Item.java
@@ -2,30 +2,32 @@ package SistemaDeGerenciamentoDeMesaDeRPG;
 
 public class Item {
 	
-	private Long number;
+	private Long id;
 	private String nome;
 	private String tipo;
 	
-	public Item(Long number, String nome, String tipo) {
-		this.number = number;
+	public Item(Long id, String nome, String tipo) {
+		this.id = id;
 		this.nome = nome;
 		this.tipo = tipo;
 	}
 	
 	public Long getNumber() {
-		return this.number;
+		return this.id;
 	}
 	
-	public void setNumber(Long number) {
-		if(number >= 0) {
-			this.number = number;
+	public void setNumber(Long id) {
+		if(id >= 0) {
+			this.id = id;
 		} else {
-			throw new IllegalArgumentException("Number inválido!");
+			throw new IllegalArgumentException("ID inválido!");
 		}
 	}
+	
 	public String getNome() {
 		return this.nome;
 	}
+	
 	public void setNome(String nome) {
 		if (nome != null && !nome.isEmpty()) {
 			this.nome = nome;
@@ -33,9 +35,11 @@ public class Item {
 			throw new IllegalArgumentException("Nome inválido");
 		}
 	}
+	
 	public String getTipo() {
 		return this.tipo;
 	}
+	
 	public void setTipo(String tipo) {
 		if (tipo != null && !tipo.isEmpty()) {
 			this.tipo = tipo;
